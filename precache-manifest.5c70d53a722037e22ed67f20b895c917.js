d135b9d5d77448650fe72fc658204494c7bc8c09
unclesnuff <60123694+unclesnuff@users.noreply.github.com>
2020-04-01 19:53:34 -0400

    Updates

---
+++ precache-manifest.5c70d53a722037e22ed67f20b895c917.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54698a0fea96b88945efbb9bcc3a2a45",
+    "url": "/thinkthonk/index.html"
+  },
+  {
+    "revision": "92aac9dd3d1e0e042d85",
+    "url": "/thinkthonk/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e935dc84bb3bd00b13e1",
+    "url": "/thinkthonk/static/js/2.44109e1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thinkthonk/static/js/2.44109e1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92aac9dd3d1e0e042d85",
+    "url": "/thinkthonk/static/js/main.a6176483.chunk.js"
+  },
+  {
+    "revision": "540d4f51e129417aa5d6",
+    "url": "/thinkthonk/static/js/runtime-main.29c28727.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/thinkthonk/static/media/logo.5d5d9eef.svg"
+  }
+]);
