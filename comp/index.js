26ea16a27f3644c56ab70d98ae9e4a2297684b94
unclesnuff <60123694+unclesnuff@users.noreply.github.com>
2020-04-01 20:15:52 -0400

    .

---
+++ comp/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Comp = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Comp.propTypes = {
+
+};
+
+export default Comp;
