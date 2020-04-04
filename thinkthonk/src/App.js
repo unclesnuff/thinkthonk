c7197ac0c179c65d2a29f1492e81372746137e56
unclesnuff <60123694+unclesnuff@users.noreply.github.com>
2020-04-04 14:49:09 -0400

    init

--- thinkthonk/src/App.js
+++ thinkthonk/src/App.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>TESTING</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div className="App">
+<div class="container"></div></div>
+  }
 }
 
 export default App;
