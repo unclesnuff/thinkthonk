83bf3b60ca75a233ad849839f6b92b7287790062
unclesnuff <60123694+unclesnuff@users.noreply.github.com>
2020-04-04 14:48:53 -0400

    init

---
+++ thinkthonk/src/header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Header = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Header.propTypes = {
+
+};
+
+export default Header;
