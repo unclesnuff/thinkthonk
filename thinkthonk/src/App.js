5632c3a039eefdda109bf201daedc9803dc9021f
unclesnuff <60123694+unclesnuff@users.noreply.github.com>
2020-04-04 15:04:27 -0400

    restructured app & index.js files

--- thinkthonk/src/App.js
+++ thinkthonk/src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 class App extends Component {
   render() {
     return <div className="App">
-<div class="container"></div></div>
-  }
+<div class="container">
+<h1>FUCK</h1>
+</div></div>
+
+
+}
 }
 
 export default App;
--- thinkthonk/src/index.js
+++ thinkthonk/src/index.js
@@ -15,8 +15,8 @@ ReactDOM.render(
 );
 
 ReactDOM.render(
-<App />, document.getElementById('root')
-)
+  <App />, document.getElementById('root')
+  )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
